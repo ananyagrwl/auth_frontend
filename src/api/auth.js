8ddb06eeaa02de7054545d6c71111a8c967481a2
ananyagrwl <ananya88agrawal@gmail.com>
2024-04-19 20:20:23 +0530

    signup complete

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const API = "http://localhost:5000/";
 
-export const signup = async (name, email, password, age, location, position, department ) => {
+export const signup = async (name, email, password, age, location, position, department) => {
     try {
-        const data = await axios.post(API+'signup', { name, email, password, age, location, position, department});
-        if (data){
+        const data = await axios.post(API + 'signup', { name, email, password, age, location, position, department });
+        if (data) {
             return data.data;
         }
-        else{
+        else {
             return null
         }
     } catch (error) {
@@ -16,13 +16,13 @@ export const signup = async (name, email, password, age, location, position, dep
     }
 }
 
-export const verifyotp = async (email ) => {
+export const verifyotp = async (email) => {
     try {
-        const data = await axios.post(API+'otp', { email});
-        if (data){
+        const data = await axios.post(API + 'otp', { email });
+        if (data) {
             return data;
         }
-        else{
+        else {
             return null
         }
     } catch (error) {
@@ -30,14 +30,29 @@ export const verifyotp = async (email ) => {
     }
 }
 
-export const login = async (email, password ) => {
+export const login = async (email, password) => {
     try {
-        const data = await axios.post(API+'login', {email, password});
-        if (data.data){
-            
+        const data = await axios.post(API + 'login', { email, password });
+        if (data.data) {
+
             return data.data;
         }
-        else{
+        else {
+            return null
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const jwtverify = async (token) => {
+    try {
+        const data = await axios.post(API + `fetch/${token}`);
+        if (data) {
+            console.log("jwtverify", data);
+            return data.data;
+        }
+        else {
             return null
         }
     } catch (error) {
